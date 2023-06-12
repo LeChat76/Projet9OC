29c09b1a5c683d7b05264fe7a63c913727cba256
LeChat76 <cedrik76@msn.com>
2023-06-12 18:02:11 +0200

    correction du signup et debut de la suite

--- litreview/authentication/forms.py
+++ litreview/authentication/forms.py
@@ -2,17 +2,39 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=63, label='Nom d’utilisateur')
-    password = forms.CharField(max_length=63, widget=forms.PasswordInput())
+    username = forms.CharField(max_length=63, label="Nom d'utilisateur")
+    password = forms.CharField(max_length=63, label="Mot de passe", widget=forms.PasswordInput())
 
 class SignupForm(UserCreationForm):
-    # delete help_text for second password fields (for my point of view : not needed)
+
+    username = forms.CharField(label="Nom d'utilisateur")
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'autocomplete': 'off'})
+        self.fields['password1'].widget.attrs.update({'autocomplete': 'off'})
+        # delete help_text for second password fields (for my point of view : not needed)
         self.fields['password2'].help_text = None
+        self.fields['password2'].widget.attrs.update({'autocomplete': 'off'})
+
     class Meta(UserCreationForm.Meta):
-        pass
         model = get_user_model()
         fields = ('username', 'password1', 'password2')
+        error_messages = {
+            'username': {
+                'unique': "Ce nom d'utilisateur est déjà utilisé. Veuillez en choisir un autre."
+            },
+        }
+
+    def clean_password2(self):
+
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Les mots de passe ne correspondent pas.")
+        
+        return password2    
